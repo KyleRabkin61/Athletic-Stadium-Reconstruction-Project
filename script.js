fae4aae267cfd9ee0fc604832ccc9c5b285c043f
KyleRabkin61 <kylerabkin6161@gmail.com>
2025-04-01 22:42:51 -0400

    Fixed animation so it only show once

--- script.js
+++ script.js
@@ -52,8 +52,6 @@ if ("IntersectionObserver" in window) {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("show");
-            } else {
-                entry.target.classList.remove("show");
             }
         });
     });
